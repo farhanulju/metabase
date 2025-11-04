70ed9614c3dd44b5b52247cc2acae2180ab6b0f9
Timofei <timofei@metabase.com>
2025-11-04 11:55:25 +0400

    Update how handlers are setting up

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -577,6 +577,9 @@ export interface SimpleDataPickerProps {
 
 export const PLUGIN_EMBEDDING_SDK = {
   isEnabled: () => false,
+  onBeforeRequestHandlers: {
+    getOrRefreshSessionHandler: async () => {},
+  },
 };
 
 export const PLUGIN_EMBEDDING_IFRAME_SDK = {
